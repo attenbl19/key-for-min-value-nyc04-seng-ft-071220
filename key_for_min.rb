6f53a20d33b3013b8ab195b96819b52a52ec2d48
Lida Attenborough <attenboroughl@protonmail.com>
2020-07-21 16:07:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_key = {}
   lowest_value = {}
   hash.each do |key, value|
-    if lowest_value == nil || lowest_value
+    if lowest_value == nil || value < lowest_value
     end
     lowest_key
   end
